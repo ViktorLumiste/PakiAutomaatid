ff94f503f0da57f89db560381c055f2e4774f8c5
ViktorLumiste <viktor.lumiste@gmail.com>
2023-05-30 10:07:16 +0300

    Paki automaatid

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
 import './App.css';
 
 function App() {
+  const [pakiautomaadid, setPakiautomaadid] = useState([]);
+
+  useEffect(() => {
+    fetch("http://localhost:3000/parcel-machines")
+        .then(res => res.json())
+        .then(json => setPakiautomaadid(json));
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <select>
+          {pakiautomaadid.map(automaat =>
+              <option>
+                {automaat.NAME}
+              </option>)}
+        </select>
+      </div>
   );
 }
 
