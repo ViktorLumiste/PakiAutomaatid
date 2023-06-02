9734cc058e90170e5ccc0b1df6d3753e32820eee
ViktorLumiste <viktor.lumiste@gmail.com>
2023-06-02 09:57:20 +0300

    Pakiautomaadid

--- src/index.js
+++ src/index.js
@@ -4,8 +4,12 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import cors from "cors";
+import express from "express";
+const app = express();
 
-app.use(cors())
+app.use(cors({
+    origin: ['http://localhost:3000']
+}));
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
